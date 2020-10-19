395d52b07b8981683de4a253b3921de762052399
Adrian <amustelier@outlook.com>
2020-10-19 07:15:46 +0200

    Add HttpMiddleware

---
+++ samples/BlGrid.ServerSide/HttpContextMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using DnetSpinnerComponent.Infrastructure.Interfaces;
+
+namespace BlGrid.ServerSide
+{
+    public class HttpContextMiddleware : DelegatingHandler
+    {
+        private ISpinnerService _spinnerService;
+
+        private volatile bool _disposed = false;
+
+        public HttpContextMiddleware(ISpinnerService spinnerService)
+        {
+            _spinnerService = spinnerService;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            _spinnerService.UdateCounter(1);
+
+            return base.SendAsync(request, cancellationToken);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && !_disposed)
+            {
+                _disposed = true;
+                //_spinnerService.UdateCounter(-1);
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
